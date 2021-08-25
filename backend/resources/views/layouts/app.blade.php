cc954895f2126f258e734921e0e23678263d1a32
billiejoenahema <billiejoenahema@gmail.com>
2021-08-25 09:56:32 +0900

    remove: unnecessary padding

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -35,11 +35,6 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -79,23 +74,29 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main>
             <div class="row">
-                <div class="col-md-2">
+                <div class="col-md-2 p-0">
                     <div class="card">
-                        <div class="card-header">
-                            Featured
-                        </div>
+                        <div class="card-header">左カラム</div>
                         <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                            <p class="card-text">
+                                With supporting text below as a natural lead-in to additional content.
                             </p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">中央カラム</div>
-                <div class="col-md-6">
+                <div class="col-md-4 p-0">
+                    <div class="card">
+                        <div class="card-header">中央カラム</div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                With supporting text below as a natural lead-in to additional content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 p-0">
                     右カラム
                     @yield('content')
                 </div>
