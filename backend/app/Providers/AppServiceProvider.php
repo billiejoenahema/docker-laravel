ea75468218b8c594df0c721beeb989b93d96e5e9
billiejoenahema <billiejoenahema@gmail.com>
2021-09-03 11:37:15 +0900

    implement: narrow down memo with tags

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -28,12 +28,25 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer('*', function ($view) {
-            // get all memo
-            $memos = Memo::select('memos.*')
-                ->where('user_id', '=', \Auth::id())
-                ->whereNull('deleted_at')
-                ->orderBy('updated_at', 'DESC')
-                ->get();
+            $query_tag = \Request::query('tag');
+
+            if (!empty($query_tag)) {
+                // narrow down memo with tags
+                $memos = Memo::select('memos.*')
+                    ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+                    ->where('memo_tags.tag_id', '=', $query_tag)
+                    ->where('user_id', '=', \Auth::id())
+                    ->whereNull('deleted_at')
+                    ->orderBy('updated_at', 'DESC')
+                    ->get();
+            } else {
+                // get all memo
+                $memos = Memo::select('memos.*')
+                    ->where('user_id', '=', \Auth::id())
+                    ->whereNull('deleted_at')
+                    ->orderBy('updated_at', 'DESC')
+                    ->get();
+            }
 
             // get all tag
             $tags = Tag::where('user_id', '=', \Auth::id())
