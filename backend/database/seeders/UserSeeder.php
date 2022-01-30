844bd4476ab1357dbc1b7bee47de44bedfc418eb
billiejoenahema <billiejoenahema@gmail.com>
2022-01-31 08:19:06 +0900

    make: user seeder

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'user1',
+            'email' => 'user1@example.com',
+            'password' => Hash::make('11111111'),
+            'email_verified_at' => now(),
+        ]);
+    }
+}
