17d9e2368ebdf5e727d2193d077128159e065229
billiejoenahema <billiejoenahema@gmail.com>
2022-03-18 07:42:56 +0900

    remove: unnecessary comment

--- backend/database/migrations/2021_08_19_223715_create_memos_table.php
+++ backend/database/migrations/2021_08_19_223715_create_memos_table.php
@@ -14,7 +14,7 @@ class CreateMemosTable extends Migration
     public function up()
     {
         Schema::create('memos', function (Blueprint $table) {
-            $table->unsignedBigInteger('id', true); // = Increments
+            $table->unsignedBigInteger('id', true);
             $table->longText('content');
             $table->unsignedBigInteger('user_id');
             $table->softDeletes();
