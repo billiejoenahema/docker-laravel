5150003f199ebdbd0e247b86b98da7edc741d089
billiejoenahema <billiejoenahema@gmail.com>
2022-04-05 07:31:57 +0900

    refact: cut out determine input length is over function

--- backend/resources/js/functions/determineIsOver.js
+++ backend/resources/js/functions/determineIsOver.js
@@ -1,9 +1,10 @@
 import { MAX_LENGTH } from '../consts/maxLength';
 
-export const determineIsOver = (length) => {
-  if (MAX_LENGTH.content - 10 < length && length <= MAX_LENGTH.content) {
+export const determineIsOver = (prop, length) => {
+  const warningRate = 0.9;
+  if (MAX_LENGTH[prop] * warningRate < length && length <= MAX_LENGTH[prop]) {
     return 'warn';
-  } else if (MAX_LENGTH.content < length) {
+  } else if (MAX_LENGTH[prop] < length) {
     return 'error';
   } else {
     return '';
