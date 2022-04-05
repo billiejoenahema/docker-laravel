b233224d96677f18d424db44855ad3513dd78222
billiejoenahema <billiejoenahema@gmail.com>
2022-04-06 08:19:23 +0900

    refact: cut out memo search component

--- backend/resources/js/components/MemoList.vue
+++ backend/resources/js/components/MemoList.vue
@@ -1,26 +1,20 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 import { useStore } from 'vuex';
 import MemoItem from './MemoItem';
+import MemoSearch from './MemoSearch';
 
 const store = useStore();
 
 store.dispatch('memos/get');
 const memos = computed(() => store.getters['memos/data']);
-const searchWord = ref('');
-const search = () => {
-  store.dispatch('memos/get', { search_word: searchWord.value });
-};
 </script>
 
 <template>
   <div id="memo-list">
     <div class="list-title">メモ一覧</div>
     <div class="list-body">
-      <div class="search-input-area">
-        <input class="search-input" type="search" v-model="searchWord" />
-        <button class="search-button" @click="search()">検索</button>
-      </div>
+      <MemoSearch />
       <ul>
         <li v-for="(memo, index) in memos" :key="index">
           <MemoItem :memo="memo" />
---
+++ backend/resources/js/components/MemoSearch.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, watchEffect } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+const searchWord = ref('');
+const search = () => {
+  store.dispatch('memos/get', { search_word: searchWord.value });
+};
+watchEffect(() => {
+  if (searchWord.value === '') {
+    store.dispatch('memos/get');
+  }
+});
+</script>
+
+<template>
+  <div class="search-input-area">
+    <input class="search-input" type="search" v-model="searchWord" />
+    <button class="search-button" @click="search()">検索</button>
+  </div>
+</template>
