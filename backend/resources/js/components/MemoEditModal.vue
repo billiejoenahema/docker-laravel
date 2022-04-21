5ffba2c87d7f898c0688bdfbbc830216aa8f7ac9
billiejoenahema <billiejoenahema@gmail.com>
2022-04-22 07:49:39 +0900

    feat: show tags in memo edit modal

--- backend/resources/js/components/MemoEditModal.vue
+++ backend/resources/js/components/MemoEditModal.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { computed } from '@vue/reactivity';
-import { reactive } from 'vue';
+import { computed, reactive } from 'vue';
 import { useStore } from 'vuex';
+import TagSelectedItem from '../components/TagSelectedItem';
 
 const store = useStore();
 const props = defineProps({
@@ -9,6 +9,7 @@ const props = defineProps({
     id: 0,
     title: '',
     content: '',
+    tags: [],
     created_at: null,
     updated_at: null,
   },
@@ -21,6 +22,7 @@ const memo = reactive({
   id: props.currentMemo.id,
   title: props.currentMemo.title,
   content: props.currentMemo.content,
+  tags: props.currentMemo.tags,
   created_at: props.currentMemo.created_at,
   updated_at: props.currentMemo.updated_at,
 });
@@ -37,15 +39,20 @@ const updateMemo = async () => {
 <template>
   <div class="modal" @click.self="closeModal()">
     <div class="memo-edit-area">
+      <TagSelectedItem
+        v-for="(tag, index) in memo.tags"
+        :key="tag.id"
+        :tag="tag"
+        :index="index"
+      />
+      <button>タグを追加する</button>
       <div class="date-area">
         <div class="date">作成日時: {{ memo.created_at }}</div>
         <div class="date">更新日時: {{ memo.updated_at }}</div>
       </div>
       <input class="memo-title-edit-input" v-model="memo.title" />
       <textarea v-model="memo.content" rows="12"></textarea>
-      <div class="edit-button-area">
-        <button class="edit-button" @click="updateMemo()">更新する</button>
-      </div>
+      <button class="edit-button" @click="updateMemo()">更新する</button>
     </div>
   </div>
 </template>
