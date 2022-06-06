5fc6ef534e99b413922b3dba15013f637d0b39a2
billiejoenahema <billiejoenahema@gmail.com>
2022-06-07 08:23:58 +0900

    feat: make memo&tag factory

---
+++ backend/database/factories/MemoFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Memo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MemoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Memo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => 1,
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->sentence,
+        ];
+    }
+}
